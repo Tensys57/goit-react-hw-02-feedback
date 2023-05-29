1c3cef978729b8f065e652ced02f04be290e4b9a
Tensys57 <tensys57@gmail.com>
2023-05-30 02:41:58 +0300

    Part 1 - except components

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import css from './Feedback.module.css';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
 
-export class Feedback extends Component {
-  // static defaultState = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // };
-
+export class Feedback extends React.Component {
   state = {
     good: 0,
     neutral: 0,
@@ -52,46 +48,21 @@ export class Feedback extends Component {
 
   render() {
     const { good, neutral, bad } = this.state;
+
     return (
-      <div>
-        <h4 className={css.title}>Please leave feedback</h4>
-        <div className={css.buttonBox}>
-          <button
-            type="button"
-            className={css.feedbackBtn}
-            name="good"
-            onClick={this.onClickHandler}
-          >
-            Good
-          </button>
-          <button
-            type="button"
-            className={css.feedbackBtn}
-            name="neutral"
-            onClick={this.onClickHandler}
-          >
-            Neutral
-          </button>
-          <button
-            type="button"
-            className={css.feedbackBtn}
-            name="bad"
-            onClick={this.onClickHandler}
-          >
-            Bad
-          </button>
-        </div>
-        <div className={css.statBox}>
-          <h4 className={css.title}>Statistics</h4>
-          <p className={css.statTitle}>Good: {good}</p>
-          <p className={css.statTitle}>Neutral: {neutral}</p>
-          <p className={css.statTitle}>Bad: {bad}</p>
-          <p className={css.statTitle}>Total: {this.countTotalFeedback()}</p>
-          <p className={css.statTitle}>
-            Positive feedback: {this.countPositiveFeedbackPercentage()}%
-          </p>
-        </div>
-      </div>
+      <>
+        <FeedbackOptions
+          options={this.state}
+          onLeaveFeedback={this.onClickHandler}
+        />
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback}
+          positivePercentage={this.countPositiveFeedbackPercentage}
+        />
+      </>
     );
   }
 }
